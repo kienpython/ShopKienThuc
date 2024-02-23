d122439b63121cdd6fa212bdb5d93abecc5dc5b0
Pham Ngoc Kien <kienpndev@gmail.com>
2024-02-23 20:02:29 +0700

    done init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [listData, setListData] = useState([]);
+  // Lấy dữ liệu từ link api
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/")
+      .then((res) => {
+        setListData(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(listData);
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+Home.propTypes = {
+  listData: PropTypes.func,
+};
+
+function Home(props) {
+  return <div></div>;
+}
+
+export default Home;
